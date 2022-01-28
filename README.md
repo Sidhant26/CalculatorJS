f0be3f82c81de374b84a1a15c8471263b345a560
Sidhant <sidhant26sood@gmail.com>
2022-01-28 18:33:24 +0530

    Updated readme, added all the JS
    Completed the entire calculator

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CalculatorJS
 
 Creating a basic calculator using HTML, CSS and Javascript.
+
+This mini-project was created to become familiar with Javascript, especially DOM manipulation and events, and also to get experience with how to handle different classes and learn about order of priority in CSS.
--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let firstDisplay="";
 let secondDisplay="";
 let hasDecimal=false;       /* to prevent the display from having multiple decimal points*/
 let lastOp="";              /* last operation*/
-let finalResult="";
+let finalResult="";         /* temporary result  to be used in operations*/
 
 numbers.forEach(no=>{
 no.addEventListener("click",(e)=>{
@@ -21,15 +21,69 @@ no.addEventListener("click",(e)=>{
     secondDisplay+=e.target.innerText;
     curr.innerText=secondDisplay;
     })
-})
+});
 
-operations.forEach(op=>{
-    operations.addEventListener("click",(e)=>{
+operations.forEach((op)=>{
+    op.addEventListener("click",(e)=>{
         if(!secondDisplay)
         return;
         hasDecimal=false;           /* because the new number we input after clicking an operation won't have a decimal*/
         let operationname=e.target.innerText;
-        if(firstDisplay && secondDisplay && lastOp)
+        if(firstDisplay && secondDisplay && lastOp) /* if true, it means we have entered a new operand*/
         doOperation();
+        else
+        finalResult=parseFloat(secondDisplay);  /* because the display has a string, not a number*/
+        clear(operationname);
+        lastOp=operationname;
     })
-})
+});
+
+function clear(v="")           /* moves whatever is shown on the "main" display to the first line in the display*/
+{
+    firstDisplay+=secondDisplay+" "+v+" ";      /*adds previous operand*/
+    prev.innerText=firstDisplay;
+    curr.innerText="";
+    secondDisplay="";
+}
+
+function doOperation()
+{
+    parseFloat(finalResult);
+    parseFloat(secondDisplay);
+    switch(lastOp)
+    {
+        case "/":finalResult=parseFloat(finalResult)/parseFloat(secondDisplay);
+                 break;
+        case "*":finalResult=parseFloat(finalResult)*parseFloat(secondDisplay);
+                 break;
+        case "+":finalResult=parseFloat(finalResult)+parseFloat(secondDisplay);
+                 break;
+        case "-":finalResult=parseFloat(finalResult)-parseFloat(secondDisplay);
+                 break;
+        case "%":finalResult=parseFloat(finalResult)%parseFloat(secondDisplay);
+                 break;
+    }
+}
+
+equals.addEventListener("click",(e)=>{
+    if(!firstDisplay || !secondDisplay)
+    return;                             /* nothing to show in the display*/
+    hasDecimal=false;
+    doOperation();
+    clear();
+    prev.innerText=finalResult;
+    secondDisplay=finalResult;
+    firstDisplay="";
+});
+
+allclear.addEventListener("click",(e)=>{
+    curr.innerText="0";
+    prev.innerText="0";
+    firstDisplay="";
+    secondDisplay="";
+});
+
+del.addEventListener("click",(e)=>{
+    curr.innerText="0";
+    secondDisplay="";
+});
